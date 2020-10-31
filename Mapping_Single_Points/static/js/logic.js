fdd87072df4854c9e993bfd8874959daf31325a6
Andrew Sanchez <asanchez116@gmail.com>
2020-10-31 09:24:21 -0500

    multiple map points

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -19,6 +19,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // with https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/
 // remove png from url 
 
+
 //  Add a marker to the map for Los Angeles, California.
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
@@ -26,11 +27,11 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     radius: 100
 //  }).addTo(map);
 
-let circle = L.circleMarker([34.0522, -118.2437],{
-    radius: 100, 
-    color: "black", 
-    fillColor: "#ffffa1"
-});
+// let circle = L.circleMarker([34.0522, -118.2437],{
+//     radius: 100, 
+//     color: "black", 
+//     fillColor: "#ffffa1"
+// });
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
