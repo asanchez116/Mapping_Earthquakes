cc87e9c8e4b28a68aafc02fa8c31317d68f6801e
Andrew Sanchez <asanchez116@gmail.com>
2020-10-31 12:26:31 -0500

    added map lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,35 +2,40 @@
 // console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([39.8283, -98.5795], 4.5);
 
 
 
-// Get data from cities.js
-let cityData = cities;
+// Coordinates for each point to be used in the line.
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790]
+//   ];
 
+// Coordinates for each point to be used in the polyline.
 
-// Loop through the cities array and create one marker for each city.
-// Add popup details for each marker
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     L.marker(city.location)
-//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//   .addTo(map);
-// });
+let line = [
+    [37.6213, -122.3790],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6413, -73.7781],
+    [30.274178, -89.777057]
+  ];
 
-// Make a circle marker with radius relative to pop size 
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+// // Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//     color: "red"
+//   }).addTo(map);
 
+// Create a polyline using the line coordinates and make the line black.
+L.polyline(line, {
+    color: "blue",
+    weight: 5, 
+    opacity: 0.5,
+    dashArray: '5, 10',
+ }).addTo(map);
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
