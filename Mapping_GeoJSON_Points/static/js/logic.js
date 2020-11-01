523afdb40521fddc42bf50f573a6c4b5119a4869
Andrew Sanchez <asanchez116@gmail.com>
2020-10-31 21:17:51 -0500

    togle dark and streets map

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -5,7 +5,9 @@
 // console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+// let map = L.map('mapid').setView([30, 30], 2);
+
+
 
 // // Add GeoJSON data.
 // let sanFranAirport =
@@ -26,8 +28,6 @@ let map = L.map('mapid').setView([30, 30], 2);
 //             "type":"Point",
 //             "coordinates":[-122.375,37.61899948120117]}}
 // ]};
-
-
 
 // L.geoJSON(sanFranAirport).addTo(map);
 
@@ -55,8 +55,32 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// streets.addTo(map);
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
 
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/asanchez116/Mapping_Earthquakes/main/majorAirports.json";
